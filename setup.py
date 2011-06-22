a851b359185132645d9dce3d11037f3263604358
phil <phil@secdev.org>
2011-06-23 00:16:56 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'Grandalf',
+    version = '0.1',
+    packages=['grandalf'],
+    # Metadata
+    author = 'Axel Tillequin',
+    author_email = 'bdcht3@gmail.com',
+    description = 'Dynamic 2D graph placement library',
+    license = 'GPLv2',
+    # keywords = '',
+    # url = '',
+)
