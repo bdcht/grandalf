95a2e915fff22b2e1a4c45fcb53d26e499a7bb6a
Axel Tillequin <bdcht3@gmail.com>
2011-08-24 13:36:25 +0200

    parser for the dot file format included in utils

--- grandalf/utils.py
+++ grandalf/utils.py
@@ -285,3 +285,349 @@ def setroundcorner(e,pts):
         else:
             break
     return splines or [[P[0],P[-1]]]
+
+#------------------------------------------------------------------------------
+# LALR(1) parser for Graphviz dot file format.
+class Dot:
+
+    _reserved = (
+        'strict',
+        'graph',
+        'digraph',
+        'subgraph',
+        'node',
+        'edge',
+    )
+    _tokens = (
+        'regulars',
+        'string',
+        'comment',
+    )+_reserved
+
+    _literals = [',',';','-','>','=',':','[',']','{','}']
+
+    class Lexer(object):
+        def __init__(self):
+            self.whitespace = '\0\t\n\f\r '
+            self.reserved = Dot._reserved
+            self.tokens = Dot._tokens
+            self.literals = Dot._literals
+            self.t_ignore = self.whitespace
+    
+        def t_regulars(self,t):
+            r'[A-Za-z0-9_]+'
+            v = t.value.lower()
+            if v in self.reserved:
+                t.type = v
+                return t
+            return t
+      
+        def t_comment_online(self,t):
+            r'(//(.*)\n)|\\\n'
+            pass
+      
+        def t_comment_multline(self,t):
+            r'(/\*)'
+            start=t.lexer.lexpos
+            t.lexer.lexpos += t.lexer.lexdata.index('*/',start)+2
+      
+        def t_string(self,t):
+            r'"'
+            start=t.lexer.lexpos-1
+            i = t.lexer.lexdata.index('"',start+1)
+            while t.lexer.lexdata[i-1] =='\\' :
+                i = t.lexer.lexdata.index('"',i+1)
+            t.value = t.lexer.lexdata[start:i+1]
+            t.lexer.lexpos = i+1
+            return t
+      
+        def t_ANY_error(self,t):
+            print "Illegal character '%s'" % t.value[0]
+            t.lexer.skip(1)
+    
+        def build(self,**kargs):
+            from ply.lex import lex
+            self._lexer = lex(module=self, **kargs)
+    
+        def test(self,data):
+            self._lexer.input(data)
+            while 1:
+                tok = self._lexer.token()
+                if not tok: break
+                print tok
+    
+    # Classes for the AST returned by Parser: 
+    class ctx(object):
+        def __init__(self,c):
+            if c is None: 
+                self.g = {}
+                self.n = {}
+                self.e = {}
+            else:
+                self.g = c.g.copy()
+                self.n = c.n.copy()
+                self.e = c.e.copy()
+    class att(object):
+        def __init__(self,name,val):
+            self.name = name
+            self.val = val
+    class graph(object):
+        def __init__(self,name,strict=None,direct=None):
+            self.name = name
+            self.strict = strict
+            self.direct = direct
+            self.nodes = {}
+        def __repr__(self):
+                return '<%s instance at %x, name: %s, %d nodes>'%(
+                       self.__class__,
+                       id(self),
+                       self.name,
+                       len(self.nodes))
+    class edge(object):
+        def __init__(self,n1,n2,c):
+            self.n1 = n1
+            self.n2 = n2
+            self.p1 = n1.lastport
+            self.p2 = n2.lastport
+            self.c  = c.e
+            self.atr = {}
+    class node(object):
+        def __init__(self,name,c):
+            self.name = name
+            self.c  = c.n
+            self.atr = {}
+    
+    class Parser(object):
+        def __init__(self):
+            self.currentg = None
+            self.ctxstack = []
+            self.currentc = None
+            self.subc     = None
+            self.tokens   = Dot._tokens
+
+        def __makelist(self,p):
+            N=len(p)
+            L=[]
+            if N>2:
+                L = p[1]
+            else:
+                L = []
+            L.append(p[N-1])
+            p[0] = L
+    
+        def p_Blocks(self,p):
+            '''Blocks : Blocks graphdef
+                      | graphdef'''
+      
+            self.__makelist(p)
+    
+        def p_graphdef(self,p):
+            '''graphdef : graphdecl Block'''
+            assert self.subc != None
+            self.currentg.c = self.subc.g 
+            p[0] = self.currentg
+            self.currentg = None
+            assert len(self.ctxstack)==0
+            assert self.currentc is None
+            self.subc = None
+            print 'graph object %s created'%p[0].name
+    
+        def p_graphdecl_strict(self,p):
+            '''graphdecl : strict graph regulars
+                         | strict graph string
+                         | strict graph '''
+            assert self.currentg is None
+            try:
+                self.currentg = Dot.graph(p[3],strict=1,direct=0)
+            except IndexError:
+                self.currentg = Dot.graph('',strict=1,direct=0)
+    
+        def p_graphdecl(self,p):
+            '''graphdecl : graph regulars
+                         | graph string
+                         | graph '''
+            assert self.currentg is None
+            try:
+                self.currentg = Dot.graph(p[2],strict=0,direct=0)
+            except IndexError:
+                self.currentg = Dot.graph('',strict=0,direct=0)
+    
+        def p_digraphdecl_strict(self,p):
+            '''graphdecl : strict digraph regulars
+                         | strict digraph string '''
+            assert self.currentg is None
+            try:
+                self.currentg = Dot.graph(p[3],strict=1,direct=1)
+            except IndexError:
+                self.currentg = Dot.graph('',strict=1,direct=1)
+    
+        def p_digraphdecl(self,p):
+            '''graphdecl : digraph regulars
+                         | digraph string '''
+            assert self.currentg is None
+            try:
+                self.currentg = Dot.graph(p[2],strict=0,direct=1)
+            except IndexError:
+                self.currentg = Dot.graph('',strict=0,direct=1)
+       
+        def p_blockopen(self,p):
+            '''blockopen : '{' '''
+            self.ctxstack.append(self.currentc)
+            self.newc = Dot.ctx(self.currentc)
+            self.currentc = self.newc
+    
+        def p_Block(self,p):
+            '''Block : blockopen statements '}' '''
+            self.subc = self.currentc
+            self.currentc = self.ctxstack.pop()
+            self.currentg.edges = filter(None,p[2])
+    
+        def p_statements(self,p):
+            '''statements : statements stmt
+                          | stmt 
+                          | '''
+            self.__makelist(p)
+    
+        def p_comment(self,p):
+            '''stmt : comment'''
+            pass
+    
+        def p_subgraph(self,p):
+            '''stmt : subgraph regulars Block
+                    | subgraph string Block
+                    | Block '''
+            if len(p)>2: 
+                assert self.subc != None
+                self.subc.name = p[2]
+            self.subc = None
+    
+        def p_assign(self,p):
+            '''assign : regulars '=' string
+                      | regulars '=' regulars '''
+            p[0] = Dot.att(p[1],p[3])
+    
+        def p_stmt_graph(self,p):
+            '''stmt : assign ';' '''
+            self.currentc.g[p[1].name] = p[1].val
+      
+        def p_stmt_graph_decl(self,p):
+            '''stmt : graph '[' slist ']' ';' '''
+            for a in p[3]:
+                self.currentc.g[a.name] = a.val
+      
+        def p_stmt_node(self,p):
+            '''stmt : node '[' slist ']' ';' '''
+            for a in p[3]:
+                self.currentc.n[a.name] = a.val
+      
+        def p_stmt_edge(self,p):
+            '''stmt : edge '[' slist ']' ';' '''
+            for a in p[3]:
+                self.currentc.e[a.name] = a.val
+      
+        def p_stmt_n(self,p):
+            '''stmt : regulars ';' '''
+            n = self.currentg.nodes.get(p[1],Dot.node(p[1],self.currentc))
+            self.currentg.nodes[n.name]=n
+      
+        def p_stmt_n_l(self,p):
+            '''stmt : regulars '[' slist ']' ';' '''
+            n = self.currentg.nodes.get(p[1],Dot.node(p[1],self.currentc))
+            self.currentg.nodes[n.name]=n
+            for a in p[3]: n.atr[a.name] = a.val
+      
+        def p_slist(self,p):
+            '''slist : slist ',' assign
+                     | slist assign
+                     | assign '''
+            self.__makelist(p)
+      
+        def p_stmt_e(self,p):
+            '''stmt : chain ';' 
+                    | link  ';' '''
+            p[0] = p[1]
+    
+        def p_stmt_e_c(self,p):
+            '''stmt : chain '[' slist ']' ';' 
+                    | link  '[' slist ']' ';' '''
+            for e in p[1]: 
+                for a in p[3]: e.atr[a.name]=a.val
+            p[0] = p[1]
+      
+        def p_node_port(self,p):
+            '''nodedecl : regulars ':' regulars'''
+            n = self.currentg.nodes.get(p[1],Dot.node(p[1],self.currentc))
+            self.currentg.nodes[n.name]=n
+            n.lastport = p[3]
+            p[0] = n
+      
+        def p_node_noport(self,p):
+            '''nodedecl : regulars '''
+            n = self.currentg.nodes.get(p[1],Dot.node(p[1],self.currentc))
+            self.currentg.nodes[n.name]=n
+            n.lastport = None
+            p[0] = n
+      
+        def p_chain_ug(self,p):
+            '''chain : link '-' '-' nodedecl '''
+            assert self.currentg.direct == 0
+            n1 = p[1][-1].n2
+            n2 = p[4]
+            e = Dot.edge(n1,n2,self.currentc)
+            p[1].append(e)
+            p[0] = p[1]
+      
+        def p_chain_dg(self,p):
+            '''chain : link '-' '>' nodedecl '''
+            assert self.currentg.direct == 1
+            n1 = p[1][-1].n2
+            n2 = p[4]
+            e = Dot.edge(n1,n2,self.currentc)
+            p[1].append(e)
+            p[0] = p[1]
+        
+        def p_link_ug(self,p):
+            '''link : nodedecl '-' '-' nodedecl '''
+            assert self.currentg.direct == 0
+            n1 = p[1]
+            n2 = p[4]
+            p[0] = [Dot.edge(n1,n2,self.currentc)]
+      
+        def p_link_dg(self,p):
+            '''link : nodedecl '-' '>' nodedecl '''
+            assert self.currentg.direct == 1
+            n1 = p[1]
+            n2 = p[4]
+            p[0] = [Dot.edge(n1,n2,self.currentc)]
+      
+        def p_error(self,p):
+            print 'Syntax Error',p
+            self._parser.restart()
+
+        def build(self,**kargs):
+            from ply.yacc import yacc
+            opt=dict(debug=0,write_tables=0)
+            opt.update(**kargs)
+            self._parser = yacc(module=self,**opt)
+    
+    def __init__(self,**kargs):
+        self.lexer  = Dot.Lexer()
+        self.parser = Dot.Parser()
+
+    def parse(self,data):
+        try:
+            self.parser._parser.restart()
+        except AttributeError:
+            self.lexer.build()
+            self.parser.build()
+        except:
+            print 'unexpected error'
+            return None
+        L=self.parser._parser.parse(data,
+                                    lexer=self.lexer._lexer)
+        return L
+    
+    def read(self,filename):
+        f = file(filename,'r')
+        return self.parse(f.read())
+
--- tests/test-dot.py
+++ tests/test-dot.py
@@ -4,78 +4,94 @@ import  pdb
 
 from  grandalf.graphs  import *
 from  grandalf.layouts import *
-
-try:
-  from  iod.syntax.text.dot.parser import parser
-except ImportError:
-  print "test-dot.py uses the dot parser of the 'iod' package."
-  print "This parser (in 'iod/syntax/text/dot/parser.py') was not found." 
-  print "checkout the iod git tree and retry."
-  
+from  grandalf.utils   import Dot
 
 mypdb  = pdb.Pdb()
 
-L  = parser.parse(file('samples/dg10.dot').read())
+def test_lexer():
+    dot = Dot()
+    dot.lexer.build()
+    dot.lexer.test('''
+    strict diGRAPH "test" {
+      NODE [color="red"]
+      a b "c" ;
+      a -> b [label="e1"];
+      a -> "c";
+      // subgraph for node b:
+      subgraph b {
+        d -> e [label=x, color="blue" type=spline] -> f [type="polylines"];
+      }
+    }
+    grAPh dg {
+      "x" - "y" - "z"
+      /* edge attributes
+         for all digraph dg */
+      Edge [type=6 zone="x"];
+    }
+    ''')
 
-G  = []
+if __name__ == '__main__':
 
-for  ast in L:
-    print "testing graph %s :"%ast.name,
-    V = {}
-    E = []
-    for k,x in ast.nodes.iteritems():
-        try:
-            v = Vertex(x.atr['label'])
-        except (KeyError,AttributeError):
-            v = Vertex(x.name)
-        v.view = VertexViewer(10,10)
-        V[x.name] = v
-    print len(V)
-    edgelist = []
-    for e in ast.edges: edgelist.extend(e)
-    for edot in edgelist:
-        v1 = V[edot.n1.name]
-        v2 = V[edot.n2.name]
-        E.append(Edge(v1,v2))
-    #mypdb.set_trace()
-    G.append(Graph(V.values(),E))
-    print "  [%d vertices]"%G[-1].order()
-    print "  [%d groups]"%len(G[-1].C)
-    for gr in G[-1].C:
-    # Sugiyama algorithm applies only on directed acyclic graphs.
-    # Of course if gr is undirected, it just means that setting a
-    # default direction for an edge is meaningless and should not
-    # change its mathematical properties...
-    # The acyclic property is more difficult: If the graph has
-    # some cycles, we need to invert some edges to remove any cycle
-    # just for the Sugiyama drawing,
-    # and the difficulty comes from selecting a set of
-    # edges that need to be temporarily inverted.
-    #
-    # now we need to find "roots" vertices.
-    # The following algorithm finds all vertex with
-    # no incoming edge :
-        r = filter(lambda x: len(x.e_in())==0, gr.sV)
-    ## if len(r)==0, there exist at least one cycle in gr.
-    ## finding a "good" set of roots depends now on inverting
-    ## some edges. This can be done by redefining a graph_core
-    ## with only 
-        print "    . %d verts, %d root(s)"%(gr.order(),len(r))
+    L  = Dot().read('samples/dg10.dot')
 
-        if len(r)==0:
-            print 'no root found! default root is initial node.'
-            r = [gr.sV.o[0]]
+    G  = []
 
-        print 'using tarjan algorithm to find inverted_edges...'
-        L = gr.get_scs_with_feedback(r)
+    for  ast in L:
+        print "testing graph %s :"%ast.name,
+        V = {}
+        E = []
+        for k,x in ast.nodes.iteritems():
+            try:
+                v = Vertex(x.atr['label'])
+            except (KeyError,AttributeError):
+                v = Vertex(x.name)
+            v.view = VertexViewer(10,10)
+            V[x.name] = v
+        print len(V)
+        edgelist = []
+        for e in ast.edges: edgelist.extend(e)
+        for edot in edgelist:
+            v1 = V[edot.n1.name]
+            v2 = V[edot.n2.name]
+            E.append(Edge(v1,v2))
+        #mypdb.set_trace()
+        G.append(Graph(V.values(),E))
+        print "  [%d vertices]"%G[-1].order()
+        print "  [%d groups]"%len(G[-1].C)
+        for gr in G[-1].C:
+        # Sugiyama algorithm applies only on directed acyclic graphs.
+        # Of course if gr is undirected, it just means that setting a
+        # default direction for an edge is meaningless and should not
+        # change its mathematical properties...
+        # The acyclic property is more difficult: If the graph has
+        # some cycles, we need to invert some edges to remove any cycle
+        # just for the Sugiyama drawing,
+        # and the difficulty comes from selecting a set of
+        # edges that need to be temporarily inverted.
+        #
+        # now we need to find "roots" vertices.
+        # The following algorithm finds all vertex with
+        # no incoming edge :
+            r = filter(lambda x: len(x.e_in())==0, gr.sV)
+        ## if len(r)==0, there exist at least one cycle in gr.
+        ## finding a "good" set of roots depends now on inverting
+        ## some edges. 
+            print "    . %d verts, %d root(s)"%(gr.order(),len(r))
 
-        sug = SugiyamaLayout(gr)
+            if len(r)==0:
+                print 'no root found! default root is initial node.'
+                r = [gr.sV.o[0]]
 
-        sug.init_all(roots=r,inverted_edges=filter(lambda x:x.feedback, gr.sE))
-        sug.draw()
-        #for s in sug.draw_step(): pass
-        for v,x in sug.grx.iteritems():
-            label = v.data if hasattr(v,'data') else '*'
-            print label, x, v.view.xy
-        print 'Sugiyama drawing done.'.ljust(80,'_')
+            print 'using tarjan algorithm to find inverted_edges...'
+            L = gr.get_scs_with_feedback(r)
+
+            sug = SugiyamaLayout(gr)
+
+            sug.init_all(roots=r,inverted_edges=filter(lambda x:x.feedback, gr.sE))
+            sug.draw()
+            #for s in sug.draw_step(): pass
+            for v,x in sug.grx.iteritems():
+                label = v.data if hasattr(v,'data') else '*'
+                print label, x, v.view.xy
+            print 'Sugiyama drawing done.'.ljust(80,'_')
 
