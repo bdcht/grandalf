8dea83df2b2c2346db0059bc9d9cad4090cb4d22
Axel Tillequin <bdcht3@gmail.com>
2015-07-13 16:45:42 +0200

    fix utils.linalg array/matrix classes used when numpy is not found

--- grandalf/utils/linalg.py
+++ grandalf/utils/linalg.py
@@ -4,12 +4,31 @@ from array import array as _array
 
 
 def coerce_(types):
+    if types is None: types = []
     if str in types: raise TypeError
     if complex in types: raise TypeError
-    dtype = 'i'
-    if long in types: dtype = 'l'
-    if float in types: dtype = 'd'
+    dtype = ('i',int)
+    if long in types: dtype = ('l',long)
+    if float in types: dtype = ('d',float)
     return dtype
+
+def _mkslice(i,n):
+    if not isinstance(i,slice):
+        i = slice(i,i+1,1)
+    start,stop,stride = i.indices(n)
+    return slice(start,stop,stride)
+
+def make_ij_slices(f):
+    def wrapper(self,ij,*args):
+        I = slice(0,self.n,1)
+        J = slice(0,self.p,1)
+        if isinstance(ij,tuple):
+            I = _mkslice(ij[0],self.n)
+            J = _mkslice(ij[1],self.p)
+        else:
+            I = _mkslice(ij,self.n)
+        return f(self,(I,J),*args)
+    return wrapper
 
 constants = (int,long,float)
 
@@ -19,22 +38,35 @@ class array(object):
 
     def __init__(self,data,dtype=None,copy=True):
         self.dim = len(data)
-        if self.dim>0 and dtype is None:
+        types = None
+        if self.dim>0:
             types = set([type(x) for x in data])
-            dtype = coerce_(types)
-        self.dtype = dtype
+        if dtype is not None:
+            types = (dtype,)
+        tc,self.dtype = coerce_(types)
+        data = [self.dtype(x) for x in data]
         if copy is True:
-            self.data = _array(dtype,data)
+            self.data = _array(tc,data)
         else:
             raise NotImplementedError
 
     def coerce(self,dtype):
-        self.data = _array(dtype,self.data)
+        data = [dtype(x) for x in self.data]
+        tc,dtype = coerce_((dtype,))
+        self.data = _array(tc,data)
         self.dtype = dtype
+
+    @property
+    def typecode(self):
+        return self.data.typecode
 
     def __len__(self): return self.dim
 
-    def __str__(self): return str(self.data)
+    def __str__(self):
+        s = ' '.join(("%.12s"%x).ljust(12) for x in self)
+        return '[%s]'%s.strip()
+
+    def copy(self): return array(self.data,self.dtype)
 
     def __add__(self,v):
         if isinstance(v,constants):
@@ -75,27 +107,68 @@ class array(object):
         assert v.dim==self.dim
         return array([x/y for (x,y) in zip(self.data,v.data)])
 
+    def __rdiv__(self,v):
+        if isinstance(v,constants):
+            v = array([v]*self.dim)
+        assert v.dim==self.dim
+        return array([x/y for (x,y) in zip(v.data,self.data)])
+
+    def __floordiv__(self,v):
+        if isinstance(v,constants):
+            v = array([v]*self.dim)
+        assert v.dim==self.dim
+        return array([x//y for (x,y) in zip(self.data,v.data)])
+
+    def __rfloordiv__(self,v):
+        if isinstance(v,constants):
+            v = array([v]*self.dim)
+        assert v.dim==self.dim
+        return array([x//y for (x,y) in zip(v.data,self.data)])
+
     def norm(self):
         return sqrt(self.dot(self))
 
+    def max(self):
+        return max(self.data)
+    def min(self):
+        return min(self.data)
+
     def __iter__(self):
         for x in self.data: yield x
+
+    def __setitem__(self,i,v):
+        assert isinstance(i,(int,long))
+        self.data[i] = self.dtype(v)
+
+    def __getitem__(self,i):
+        i = _mkslice(i,self.dim)
+        res = self.data[i]
+        if len(res)==1: return res[0]
+        return array(res)
+
+    def transpose(self):
+        return matrix(self.data,self.dtype)
+
+    def __float__(self):
+        assert self.dim==1
+        return float(self.data[0])
 
 #------------------------------------------------------------------------------
 # minimalistic numpy.matrix replacement class used as fallback
 # when numpy is not found in geometry module
 class matrix(object):
 
-    def __init__(self,data,dtype=None,copy=True):
+    def __init__(self,data,dtype=None,copy=True,transpose=False):
         # check input data types:
         types = set([type(v) for v in data])
         if len(types)>1: raise TypeError
         t = types.pop()
         # import data:
-        if t in (int,long,float):
+        if t in constants:
             self.data = [array(data,dtype,copy)]
         else:
-            self.data = [array(v,dtype,copy) for v in izip(*data)]
+            if transpose: data = izip(*data)
+            self.data = [array(v,dtype,copy) for v in data]
         # define matrix sizes:
         self.n = len(self.data)
         sizes = set([len(v) for v in self.data])
@@ -104,14 +177,16 @@ class matrix(object):
         if dtype is None:
             # coerce types of arrays of matrix:
             types = set([v.dtype for v in self.data])
-            dtype = coerce_(types)
+            tc,dtype = coerce_(types)
             for v in self.data:
                 v.coerce(dtype)
         self.dtype = dtype
 
-    def __len__(self): return self.n
+    def __len__(self): return self.n*self.p
 
-    def __str__(self): return '\n'.join([str(v) for v in self.data])
+    def __str__(self):
+        s = '\n '.join([str(v) for v in self.data])
+        return "[%s]"%s.strip()
 
     @property
     def shape(self): return (self.n,self.p)
@@ -120,8 +195,35 @@ class matrix(object):
 
     def cvecs(self): return [array(v,self.dtype) for v in izip(*self.data)]
 
+    def copy(self): return matrix(self.data,self.dtype)
+
     def transpose(self):
-        return matrix(self.data,dtype=self.dtype)
+        return matrix(self.data,dtype=self.dtype,transpose=True)
+
+    def sum(self):
+        return sum([sum(v) for v in self.data])
+
+    @make_ij_slices
+    def __getitem__(self,ij):
+        I,J = ij
+        l = self.lvecs()[I]
+        m = matrix([v[J] for v in l])
+        if m.n==1:
+            v = m.data[0]
+            if v.dim==1: return v[0]
+            if len(l)>1: return v
+        return m
+
+    @make_ij_slices
+    def __setitem__(self,ij,v):
+        I,J = ij
+        Ri =range(I.start,I.stop,I.step)
+        Rj =range(J.start,J.stop,J.step)
+        if type(v) in constants: v = (v,)
+        value = (x for x in v)
+        for i in Ri:
+            for j in Rj:
+                self.data[i][j] = next(value)
 
     def __add__(self,m):
         if isinstance(m,constants):
@@ -140,6 +242,10 @@ class matrix(object):
     def __neg__(self):
         return matrix([-x for x in self.data],dtype=self.dtype)
 
+    def __float__(self):
+        assert self.n==1 and self.p==1
+        return self[0,0]
+
     def __radd__(self,v):
         return self+v
     def __rsub__(self,v):
@@ -147,13 +253,24 @@ class matrix(object):
 
     def __rmul__(self,k):
         if not isinstance(k,constants): raise TypeError
-        return matrix([k*v for v in self.cvecs()])
+        return matrix([k*v for v in self.data])
 
     def __mul__(self,X):
-        if isinstance(X,constants): raise TypeError
+        if isinstance(X,constants):
+            return X*self
         if isinstance(X,array):
             assert X.dim==self.p
             return array([v.dot(X) for v in self.data])
         if isinstance(X,matrix):
             assert X.n == self.p
             return matrix([self*v for v in X.cvecs()])
+
+    def __pow__(self,v):
+        S = [self]*v
+        assert len(S)>0
+        return reduce(lambda x,y: x*y,S)
+
+    def __iter__(self):
+        for l in self.data:
+            for v in l:
+                yield v
---
+++ tests/test_utils.py
@@ -0,0 +1,55 @@
+import pytest
+
+from grandalf.utils.linalg import *
+
+def test_linalg_001():
+    assert coerce_(None) == ('i',int)
+    assert coerce_([int,long]) == ('l',long)
+    assert coerce_([int,float]) == ('d',float)
+    assert coerce_([long,float]) == ('d',float)
+    with pytest.raises(TypeError) as x:
+        coerce_([complex])
+        assert x.type==TypeError
+
+def test_linalg_001():
+    v1 = array([1,2,3])
+    assert v1.dim==3
+    assert v1.typecode=='i'
+    assert v1.dtype==int
+    assert len(v1)==3
+    assert v1[1]==2
+    assert (v1+v1)[1]==4
+    assert (2*v1)[0]==2
+    assert (v1*2)[2]==6
+    assert sum(-v1+v1)==0
+    assert v1.dot(v1) == 14
+    assert (v1/v1).max()==1
+    assert (v1/v1).min()==1
+    assert v1.transpose().shape == (1,3)
+
+
+def test_linalg_002():
+    v1 = array([1,2.,3L])
+    assert v1.typecode=='d'
+    assert type(v1[0])==float
+    assert v1[0]==1.
+    assert float(v1.transpose()*v1)==v1.dot(v1)
+    m1 = matrix(v1)
+    assert float(m1*v1) == v1.dot(v1)
+    assert (2*m1)[0,1] == (m1+m1)[0,1]
+    assert (m1*2).shape == (2*m1).shape
+
+def test_linalg_003():
+    m1 = matrix([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+    assert m1.shape == (3,4)
+    assert len(m1)==3*4
+    m2 = matrix([[1,2,3,4],[5,6,7,8],[9,10,11,12]],transpose=True)
+    assert m2.shape == (4,3)
+    assert m1.transpose()[1,2] == m2[1,2]
+    assert m1[1:,1:].shape == (2,3)
+    v = m2[1:,0]
+    assert isinstance(v,array)
+    l2 = m2[1,1:]
+    assert isinstance(l2,matrix)
+    assert l2.n==1
+    assert m2[0,0]==1
